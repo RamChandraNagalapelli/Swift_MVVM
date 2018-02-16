6f203d3d9a5cda55393ff03905fc3f7bdac10ec1
nramchandra0 <ramchandra@ramchandras-mac-mini.local>
2018-02-16 12:19:44 +0530

    app hierarchy setup

---
+++ WeatherAppMVC/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  WeatherAppMVC
+//
+//  Created by ramchandra on 16/02/18.
+//  Copyright © 2018 ramchandra. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
